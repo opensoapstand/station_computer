e7b26ba30afcc00202ca2883a66241a01cf85cd3
Jason <wangsonj@gmail.com>
2020-04-27 16:06:37 -0700

    database setupscript for table relationship and scripts for insertion that would avoid duplication and should be included in the cpp library for usage

---
+++ mysql_script/drinkfill_master_db_insert.sql
@@ -0,0 +1,55 @@
+/*******************************************************
+// Drinkfill MySQL Master Script For Database Architecture 
+//
+// drinkfill_master_db_insert.sql
+// insert to the database table structures with keys to define relationships
+//
+// created: 27-04-2020
+// by: Jason Wang
+//
+// copyright 2020 by Drinkfill Beverages Ltd
+// all rights reservedname
+ ********************************************************/
+ /*sample vendor insertion*/
+insert into `vendors` (`full_address`, `name`, `contact_name`, `contact_phone`,`contact_email`,`connector`) 
+				       value 
+					  ('vancouver somewhere', 'NYPD', 'Jake Peralta', '909-000-0192', 'xxx@xxx.com', '242');
+
+select * from `vendors`;
+-- ------------------------------------------------------------------------------------------------------------- --
+/*Product table shows infomation about the drinks that we used or using or will be using*/
+insert into `product` (`name`, `vendor`,`image`,`calibration_const`,`cost_per_litre`) 
+			           value
+					  ('Elderflower Kombucha', 1, NULL, 2.19, 1.20);
+select * from `product`;
+-- -------------------------------------------------------------------------------------------------------------- --
+/*sample location insertion*/
+insert into `location` (`full_address`,`name`,`in_store_location`,`contact_name`,`contact_phone`,`contact_email`,`connector`,`price_multiplier`)
+						value
+					   ('#303 6th East Ave Vancouver', 'drinkfill', 'room #303', 'Jason Wang', '222-293-2942', 'www@drinkfill.com', '212', 1.23 );
+select * from `location`;
+-- --------------------------------------------------------------------------------------------------------------- --
+/*sample pricing insertion*/
+insert into `pricing` (`product_id`, `price_per_litre`,`location`)
+					  value
+                      (1, 2.10, 1);   
+select * from `pricing`;
+-- --------------------------------------------------------------------------------------------------------------- --
+/*sample machine insertion*/
+insert into `machine` (`type`,`location`,`number_of_drinks`,`last_maintenance`,`vendor_host`,`vendor_provider`)
+					   value
+					  (1, 1, 9, now(), 'City of vancouver', 'CCPD');
+select * from `machine`;
+-- ---------------------------------------------------------------------------------------------------------------- --
+/*sample inventory insertion*/
+insert into `inventory` (`machine_id`, `drink1`)
+			            value
+                        (1, 452.21);
+select * from `inventory`;		
+-- ---------------------------------------------------------------------------------------------------------------- --
+/*sample sales insertion*/
+insert into `sales` (`machine_id`, `date`, `amount`,`pan`, `reference`)
+					value
+                    ();	
+						
+

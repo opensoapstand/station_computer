6d182eef0d38aab5a9f21bcfaaa37dceabcf4fa9
dlondry <dlondry@foxsmithtech.com>
2020-06-12 16:56:14 -0700

    Basic state implementation

---
+++ df001/fsm/components/drink.h
@@ -0,0 +1,31 @@
+//***************************************
+//
+// drink.h
+// drink class owns the beverages
+//
+// created: 12-06-2020
+// by: Denis Londry
+//
+// copyright 2020 by Drinkfill Beverages Ltd
+// all rights reserved
+//***************************************
+
+#ifndef _DRINK__H_
+#define _DRINK__H_
+
+#include "../inc/dftypes.h"
+
+class drink
+{
+   public:
+      drink();
+      ~drink();
+
+   private:
+   // name
+   // ingredients
+   // volume
+   // image
+};
+
+#endif

e3cf33bc7176d35e9850fa4cd6a59b8fa8afa7f9
LiYan <liyan@drinkfill.com>
2020-07-08 13:39:27 -0700

    Finish table creation; File seperation for SQL operation scripts

--- pg_db/pg_script/df_db_setup.sql
+++ pg_db/pg_script/df_db_setup.sql
@@ -14,12 +14,12 @@
  * AWS may require '' EG: CREATE TABLE IF NOT EXISTS 'df_transaction.user' ...
  * USE postgres USER ACCOUNT CONNECTION to template1
  * List the name of all the databases in this server
-*/
-
+ */
 /*
  * Procedural Programming for creating a drinkfill Database
-*/
-DO $$ BEGIN CREATE TEMPORARY VIEW dbchk AS (
+ */
+DO $$ BEGIN -- DB Check
+CREATE TEMPORARY VIEW dbchk AS (
     SELECT datname
     FROM pg_database
     WHERE datname = 'drinkfill'
@@ -28,13 +28,12 @@ DO $$ BEGIN CREATE TEMPORARY VIEW dbchk AS (
 IF (
     SELECT COUNT(*)
     FROM dbchk
-) < 0 THEN -- Create the Database
-
--- create database if not exists drinkfill;
+) < 0 THEN
+/* -------- Database Creation -------- */
 CREATE DATABASE drinkfill;
 
 -- todo set up admin account
--- Create the Roles
+/* -------- Role Creation -------- */
 CREATE ROLE df_admin_Group WITH NOSUPERUSER NOCREATEDB;
 
 CREATE ROLE machine_group WITH NOSUPERUSER NOCREATEDB NOCREATEROLE;
@@ -43,8 +42,8 @@ CREATE ROLE machine_group WITH NOSUPERUSER NOCREATEDB NOCREATEROLE;
 -- Future role for maintenance mobile interaction
 -- Future role for vendor mobile interaction
 -- Change Default schema name
---ALTER DATABASE drinkfill OWNER TO df;
---ALTER DATABASE drinkfill OWNER TO postgres;
+-- ALTER DATABASE drinkfill OWNER TO df;
+-- ALTER DATABASE drinkfill OWNER TO postgres;
 -- Set permissions to groups
 GRANT ALL PRIVILEGES ON DATABASE drinkfill TO df_admin_group;
 
@@ -71,14 +70,10 @@ CREATE USER local_machine WITH PASSWORD 'machine1234';
 
 GRANT machine_group TO local_machine;
 
-/*
- * Create MAINTENANCE data storage
-*/
-
+/* -------- MAINTENANCE data storage -------- */
 CREATE SCHEMA IF NOT EXISTS df_maintenance;
 
 -- Tables will need machine_id FK for Web.  Do not need reference to machine_id locally.
-
 -- Log and describe maintenance performed
 CREATE TABLE IF NOT EXISTS df_maintenance.master_log (
     master_log_id SERIAL,
@@ -86,8 +81,8 @@ CREATE TABLE IF NOT EXISTS df_maintenance.master_log (
     maintenance_code VARCHAR(10),
     -- Create contraint or table for this.
     description TEXT,
-    last_maintenance TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY(master_log_id)
+    maintenance_provider_id last_maintenance TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY(master_log_id) -- TODO FK
 );
 
 -- Log and describe temperature
@@ -113,7 +108,10 @@ CREATE TABLE IF NOT EXISTS df_maintenance.waste_log (
 -- Log and describe dispensed liquids
 CREATE TABLE IF NOT EXISTS df_maintenance.flow_io_log (
     flow_io_log_id SERIAL,
-    flow_sensor_slot INT CHECK(flow_sensor_slot > 0 AND flow_sensor_slot < 8),
+    flow_sensor_slot INT CHECK(
+        flow_sensor_slot > -1
+        AND flow_sensor_slot < 8
+    ),
     -- 0 to 8 are sensors 1 to 9 respectively.
     flow_contents TEXT,
     flow_amount DECIMAL(2) DEFAULT 0.00,
@@ -121,14 +119,12 @@ CREATE TABLE IF NOT EXISTS df_maintenance.flow_io_log (
     PRIMARY KEY(flow_io_log_id)
 );
 
-/*
- * Change default schema for sales transaction data
-*/
-
+/* -------- TRANSACTION data storage -------- */
+-- Change default schema for sales transaction data
 ALTER SCHEMA public
 RENAME TO df_transaction;
 
--- Create User Table
+-- Create User Tables
 CREATE TABLE IF NOT EXISTS df_transaction.user_type (
     user_type_id VARCHAR(3) UNIQUE,
     user_type_description VARCHAR(100),
@@ -192,65 +188,96 @@ CREATE TABLE IF NOT EXISTS df_transaction.sales (
     -- mask pan data FOR MONERIS; removed for security
     -- reference VARCHAR(30),
     -- FOR MONERIS; removed for security
-    PRIMARY KEY(sales_id),
+    PRIMARY KEY(sales_id, location_id),
     FOREIGN KEY(location_id) REFERENCES df_transaction.machine_location(machine_location_id)
 );
-
-
 
 /*
  general information about the machine
  TODO grab serials and version from XML
  */
-CREATE TABLE IF NOT EXISTS machine (
-    id VARCHAR(255) DISTINCT NOT NULL,
+CREATE TABLE IF NOT EXISTS df_transaction.machine (
+    machine_id VARCHAR(255) UNIQUE NOT NULL,
     type VARCHAR(25),
     -- I.E keggerator vs full unit to be defined...
-    version NOT NULL VARCHAR(25),
+    version VARCHAR(25) NOT NULL,
     -- machine version
-    location_id int DEFAULT NULL,
-    inventory_level_id int DEFAULT NULL,
-    number_of_drinks int,
+    location_id INT DEFAULT NULL,
+    number_of_drinks INT,
     -- Do a count of inventory level
-    maintenance_log_id timestamp NOT NULL,
-    vendor_host varchar(100),
-    vendor_provider varchar(100),
-    PRIMARY KEY(id, type, version, location_id),
-    FOREIGN KEY(location_id) REFERENCES machine_location(id),
-    FOREIGN KEY(maintenance_log_id) REFERENCES maintenance_log(id)
+    PRIMARY KEY(machine_id),
+    FOREIGN KEY(location_id) REFERENCES df_transaction.machine_location(machine_location_id)
 );
 
-/*future implementation*/
-/*coupon code -> active time with 6 characters*/
-CREATE TABLE IF NOT EXISTS coupon (
-    coupon_id INT PRIMARY KEY,
-    code CHAR(6) UNIQUE,
-    machine_id INT,
-    percentage INT,
-    start_date TIMESTAMP,
-    end_date TIMESTAMP,
-    FOREIGN KEY(machine_id) references machine(id)
+/*
+ * Limit inserts to Admin; Try to only keep as many rows needed per device
+ * Treat inventory id/row/option as same:
+ * Select drink 1 > inventory_id = 1 > 
+ */
+CREATE TABLE IF NOT EXISTS df_transaction.inventory (
+    inventory_id SERIAL,
+    product_id INT,
+    volume NUMERIC(2) DEFAULT NULL,
+    date_refresh TIMESTAMP WITH TIME ZONE NOT NULL,
+    in_stock BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY(inventory_id)
 );
 
+-- drop table df_transaction.product CASCADE;
 /*
  Product holds a local catalog of drinks used in the machine
  Holds references to all products placed in machine before.
  */
-CREATE TABLE IF NOT EXISTS df_transaction.product_catalog (
-    product_catalog_id SERIAL,
+CREATE TABLE IF NOT EXISTS df_transaction.product (
+    product_id SERIAL,
     name VARCHAR(100) NOT NULL,
+    product_description VARCHAR(100) NOT NULL,
     vendor_id INT DEFAULT NULL,
-    calibration_const DOUBLE,
+    user_type VARCHAR(3) DEFAULT 'VED',
+    calibration_const NUMERIC(6),
     -- cost from vendor
-    cost_per_litre DOUBLE,
-    option_slot INT CHECK(numrange(-1, 8)),
+    cost_per_litre MONEY,
+    option_slot INT DEFAULT(-1) CHECK(
+        option_slot > -1
+        AND option_slot < 8
+    ),
     -- -1 means empty; 0 to 8 are slots 1 to 9 respectively.
     coupon_code_id INT,
-    coupon_code CHAR(6),
-    PRIMARY KEY(product_catalog_id),
-    FOREIGN KEY(vendor_id) REFERENCES df_transaction.users(user_id) FOREIGN KEY(coupon_code, coupon_code_id) REFERENCES df_transaction.coupon
+    PRIMARY KEY(product_id),
+    FOREIGN KEY(vendor_id, user_type) REFERENCES df_transaction.user(user_id, user_type),
+    FOREIGN KEY(coupon_code_id) REFERENCES df_transaction.coupon(coupon_id),
+    FOREIGN KEY(option_slot) REFERENCES df_transaction.inventory(inventory_id)
 );
 
+ALTER TABLE df_transaction.inventory
+ADD CONSTRAINT fk_inventory_product FOREIGN KEY(product_id) REFERENCES df_transaction.product(product_id);
+
+-- DROP TABLE df_transaction.coupon CASCADE;
+/*future implementation*/
+/*coupon code -> active time with 6 characters*/
+CREATE TABLE IF NOT EXISTS df_transaction.coupon (
+    coupon_id SERIAL,
+    code CHAR(6) UNIQUE,
+    percentage INT,
+    start_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    end_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY(coupon_id)
+);
+
+/*
+ * Product pricing reference based on product and location
+ */
+CREATE TABLE IF NOT EXISTS df_transaction.pricing (
+    product_id INT,
+    -- cost for consumers
+    price_per_litre MONEY,
+    location_id INT,
+    PRIMARY KEY(product_id),
+    FOREIGN KEY(product_id) REFERENCES df_transaction.product(product_id),
+    FOREIGN KEY(location_id) REFERENCES df_transaction.machine_location(machine_location_id)
+);
+
+/* -------- QTCreator data storage -------- */
 /*
  Create Schema for QT elements
  WHY?...can potentially be accessed with QT thread directly with no transaction information required
@@ -270,28 +297,6 @@ CREATE TABLE IF NOT EXISTS df_QT.product_image (
     PRIMARY KEY(product_image_id),
     FOREIGN KEY(product_image_id) REFERENCES df_transaction.product_catalog(product_catalog_id)
 )
-/*product pricing*/
-CREATE TABLE IF NOT EXISTS df_transaction.pricing (
-    product_id int,
-    -- cost for consumers
-    price_per_litre double,
-    location int,
-    PRIMARY KEY(product_id),
-    FOREIGN KEY(product_id) REFERENCES product(id),
-    FOREIGN KEY(location) REFERENCES location(id)
-);
-
-CREATE TABLE IF NOT EXISTS inventory_level (
-    inventory_level_id SERIAL,
-    product_id INT NOT NULL,
-    -- every time when there is a log, there should be coresponding id for what product and which machine
-    machine_id INT NOT NULL,
-    volume double default null,
-    refresh_date DATETIME NOT NULL,
-    PRIMARY KEY (id) REFERENCES FOREIGN KEY (product_id) REFERENCES product(id),
-    FOREIGN KEY (machine_id) REFERENCES machine(id)
-);
-
 /*Daily update of the revenue from the machine, get summarized revenue daily*/
 /*can utilize this table to display histogram of the progress*/
 -- create table if not exists `revenue` (`day` DATE NOT NULL,
@@ -329,11 +334,10 @@ END IF;
 
 END;
 
-$$ 
-
+$$
 /*
  * Teardown: todo integrate into create database procedural
-*/
+ */
 DO $$ DROP DATABASE IF EXISTS drinkfill;
 
 DROP USER IF EXISTS localmachine;
@@ -354,7 +358,6 @@ DROP GROUP IF EXISTS machineGroup;
 -- DROP TABLE IF EXISTS vendors;
 -- DROP TABLE IF EXISTS temperature_log;
 -- DROP TABLE IF EXISTS waste_log;
-
 END IF;
 
 END;

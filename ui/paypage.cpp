d5279700b92ee3b9cf56ce1160183495cebe4a72
LiYan <liyan@drinkfill.com>
2020-08-17 16:08:32 -0700

    Moneris code scaffolding reference from DF001 pilot

--- ui/paypage.cpp
+++ ui/paypage.cpp
@@ -21,6 +21,25 @@
 #include "dispensepage.h"
 #include "idle.h"
 
+extern int optionSelected;
+//static string Mid = "0030128909430";
+//static string pedID = "U4383005";
+//static string URL = "ipgt1.moneris.com:443";
+
+// TODO: Put information into XML
+
+static string Mid = "0030410835171"; //0030128909430 0030410835171
+static string pedID = "U4383002"; //U4383017 //U4383144 //U4383005
+static string URL = "ipg1.moneris.com:443";
+const QString tapDeclined = "Card Declined";
+const QString tapAgain = "Try Again";
+const QFont newFont("Arial", 30, QFont::Bold);
+extern QByteArray DispenseVolume;
+bool approved = false;
+bool paymentConnected = false;
+extern QString machineID;
+extern QByteArray tempValue;
+
 // CTOR
 payPage::payPage(QWidget *parent) :
     QWidget(parent),
@@ -34,7 +53,7 @@ payPage::payPage(QWidget *parent) :
     palette.setBrush(QPalette::Background, background);
     this->setPalette(palette);
 
-    /*hacky transparent button*/
+    /* HACK: transparent button*/
     ui->previousPage_Button->setStyleSheet("QPushButton { border-image: url(:/light/background.png); }");
     ui->passPayment_Button->setStyleSheet("QPushButton { border-image: url(:/light/background.png); }");
     ui->mainPage_Button->setStyleSheet("QPushButton { border-image: url(:/light/background.png); }");
@@ -47,6 +66,37 @@ payPage::payPage(QWidget *parent) :
     // Setup Reciept
     updateTotals(this->drinkDescription, this->drinkAmount, this->orderTotal);
 
+    // Payment
+    /*timer and slots setup*/{
+        timer = new QTimer(this);
+        connect(timer, SIGNAL(timeout()), this, SLOT(progressStatusLabel()));
+        timer->setInterval(500);
+
+        declineTimer = new QTimer(this);
+        connect(declineTimer, SIGNAL(timeout()), this, SLOT(declineTimer_start()));
+
+        pageUpdateTimer = new QTimer(this);
+        connect(pageUpdateTimer, SIGNAL(timeout()), this, SLOT(updatePageNumber()));
+        pageUpdateTimer->setInterval(10);
+        //pageUpdateTimer->start();
+
+        setpaymentProcess(false);
+        ui->processLabel->hide();
+        ui->declineLabel->setText(tapAgain);
+        labelSetup(ui->declineLabel, 40);
+
+        //pageSetup("Kombucha", ":/assets/kombucha.png", 5.95);
+
+        readTimer = new QTimer(this);
+        connect (readTimer, SIGNAL(timeout()), this, SLOT(readTimer_loop()));
+
+
+        goBackTimer = new QTimer(this);
+        connect(goBackTimer, SIGNAL(timeout()), this, SLOT(goBack()));
+        goBackTimer->start(60000);
+    }
+
+    paymentInit();
 }
 /*
  * Page Tracking reference
@@ -95,4 +145,201 @@ void payPage::on_mainPage_Button_clicked()
 {
     this->hide();
     idlePage->showFullScreen();
+}
+
+// Payment Section based on DF001 Prototype
+void payPage::showEvent(QShowEvent *event)
+{
+    enterPage = true;
+    progressValue = 0;
+    tempValue = "0";
+    DispenseVolume = "0";
+
+    mainPage->sendardCommand("Z");
+
+    ui->priceVolume1Button->setStyleSheet("border-image:url(:/assets/V6/ev2_ui_assets_v6-14.png); color: white;");
+    ui->priceVolume2Button->setStyleSheet("border-image:url(:/assets/V6/ev2_ui_assets_v6-15.png); color: white;");
+
+    pageUpdateTimer->start();
+    goBackTimer->start();//if no one uses the machine go back to first page
+    QWidget::showEvent(event);
+}
+
+void payPage::paintEvent(QPaintEvent *p)
+{
+    QWidget::paintEvent(p);
+}
+
+
+
+bool payPage::setpaymentProcess(bool status)
+{
+    return (paymentProcessing = status);
+}
+
+void payPage::setProgressLabel(QLabel* label, int dot)
+{
+    QString dotString = ".";
+    label->setText("Processing" + dotString.repeated(dot));
+}
+
+void payPage::setProductPrice(QString price)
+{
+    if(1 == optionSelected)
+    {
+        productSelectedPrice = price.toUtf8().constData(); //QString convert to string
+    }
+    else if (2 == optionSelected)
+    {
+        productSelectedPrice = price.toUtf8().constData();
+    }
+}
+
+void payPage::storeEvent(database_logger db, QString event)
+{
+    beverageData* curBev = mainPage->getBeverageData(optionSelected);
+    db.event_log(machineID, QDateTime::currentDateTime(), optionSelected, event, curBev->getInventoryVolume());
+
+}
+
+void payPage::stayAliveLogon()
+{
+    com.flushSerial();
+    /*logon packet to send*/
+    pktToSend = paymentPacket.ppPosGetConfigPkt(StatusType::GetLanStatus);
+    if (sendToUX410()){
+        waitForUX410();
+    }
+
+    pktResponded.clear();
+}
+
+void payPage::batchClose()
+{
+    com.flushSerial();
+    /*logon packet to send*/
+    pktToSend = paymentPacket.batchClosePkt();
+    if (sendToUX410()){
+        waitForUX410();
+    }
+
+    pktResponded.clear();
+}
+
+bool payPage::sendToUX410()
+{
+    int waitForAck = 0;
+    while (waitForAck < 3){
+        com.sendPacket(pktToSend, uint(pktToSend.size()));
+
+        //std::cout<<paymentPacket.getSendPacket();
+
+        //read back what is responded
+        pktResponded = com.readForAck();
+        readPacket.packetReadFromUX(pktResponded);
+        pktResponded.clear();
+        waitForAck++;
+        if (readPacket.getAckOrNak() == communicationPacketField::ACK)
+        {
+            return true;
+        }
+        usleep(500000);
+    }
+    return false;
+}
+
+bool payPage::paymentInit()
+{
+    paymentConnected = com.init();
+
+    /*Cancel any previous payment*/
+    pktToSend = paymentPacket.purchaseCancelPacket();
+    if (sendToUX410()){
+        waitForUX410();
+        pktResponded.clear();
+    } else {
+        return false;
+    }
+    com.flushSerial();
+
+    /*batch close packet to send*/
+    pktToSend = paymentPacket.batchClosePkt();
+    if (sendToUX410()){
+        waitForUX410();
+        pktResponded.clear();
+    } else {
+        return false;
+    }
+    com.flushSerial();
+
+    /*logon packet to send*/
+    pktToSend = paymentPacket.logonPacket();
+    if (sendToUX410()){
+        waitForUX410();
+        pktResponded.clear();
+    } else {
+        return false;
+    }
+    com.flushSerial();
+
+    /*getConfiguration packet to send*/
+    pktToSend = paymentPacket.ppPosGetConfigPkt(CONFIG_ID::MERCH_NAME);
+    if (sendToUX410()){
+        waitForUX410();
+        //merchantName = paymentPktInfo.dataField(readPacket.getPacket().data).substr(2);
+        //std::cout << merchantName<<endl;
+        pktResponded.clear();
+
+    } else {
+        return false;
+    }
+    com.flushSerial();
+
+    /*getConfiguration packet to send*/
+    pktToSend = paymentPacket.ppPosGetConfigPkt(CONFIG_ID::MERCH_ADDR);
+    if (sendToUX410()){
+        waitForUX410();
+        //merchantAddress = paymentPktInfo.dataField(readPacket.getPacket().data).substr(2);
+        //std::cout << merchantAddress<<endl;
+        pktResponded.clear();
+
+    } else {
+        return false;
+    }
+    com.flushSerial();
+
+    /*getConfiguration packet to send*/
+    pktToSend = paymentPacket.ppPosGetConfigPkt(CONFIG_ID::CON_TID);
+    if (sendToUX410()){
+        waitForUX410();
+        terminalID = paymentPktInfo.dataField(readPacket.getPacket().data).substr(2);
+        //std::cout << terminalID<<endl;
+        pktResponded.clear();
+
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+bool payPage::waitForUX410()
+{
+    bool waitResponse = false;
+    while (!waitResponse){
+
+        if(pktResponded[0] != 0x02){
+            pktResponded.clear();
+            pktResponded = com.readPacket();
+            usleep(10);
+        }
+        else {
+            //  pktResponded = com.readPacket();
+            readPacket.packetReadFromUX(pktResponded);
+            //std::cout << readPacket;
+            com.sendAck();
+            waitResponse = true;
+        }
+    }
+    return waitResponse;
 }
--- ui/paypage.h
+++ ui/paypage.h
@@ -20,6 +20,12 @@
 #include "df_util.h"
 #include "drinkorder.h"
 
+#include "../library/posm/mcommunication.h"
+#include "../library/posm/packetfromecr.h"
+#include "../library/posm/packetfromux410.h"
+#include "../library/posm/transactionPackets.h"
+#include "../library/posm/transactioninfo.h"
+
 class paySelect;
 class dispensePage;
 class idle;
@@ -37,11 +43,25 @@ public:
     void setPage(paySelect* pageSizeSelect, dispensePage* pageDispense, idle* pageIdle);
     ~payPage();
 
+    bool setpaymentProcess(bool status);
+    void setProgressLabel(QLabel* label, int dot);
+    void setProductPrice(QString price);
+
 private slots:
     void updateTotals(string drinkDescription, string drinkAmount, string orderTotal);
 
     void on_previousPage_Button_clicked();
     void on_passPayment_Button_clicked();
+
+    void declineTimer_start();
+    void on_goBackButton_clicked();
+    void progressStatusLabel();
+    void updatePageNumber();
+    void on_payButton_clicked();
+    void readTimer_loop();
+    void goBack();
+    void on_priceVolume1Button_clicked();
+    void on_priceVolume2Button_clicked();
 
     /*
      * Successful payment -> on_passPayment_Button_clicked()
@@ -49,11 +69,50 @@ private slots:
      */
     void on_mainPage_Button_clicked();
 
+protected:
+    void showEvent(QShowEvent *event);
+    void paintEvent(QPaintEvent *p);
+
 private:
     Ui::payPage *ui;
     paySelect* paySelectPage;
     dispensePage* dispensingPage;
     idle* idlePage;
+
+    bool paymentProcessing;
+
+    /*payment send & responding packet for moneris device*/
+    mCommunication com;
+    packetFromECR sendPacket;
+    packetFromUX410 readPacket;
+    transactionPacket paymentPacket;
+    transactionInfo paymentPktInfo;
+
+    /*timer*/
+    QTimer *readTimer;
+    QTimer *declineTimer;
+    QTimer* timer;
+    QTimer* pageUpdateTimer;
+    QTimer* goBackTimer;
+
+    /*prgress timer*/
+    int progressDots = 1;
+    int counter = 0;
+    int declineCounter;
+
+    std::vector<uint8_t> pktToSend;
+    std::vector<uint8_t> pktResponded;
+
+    bool purchaseEnable;
+    int monerisConfig = 0;
+    bool timerEnabled;
+    std::string productSelectedPrice;
+    bool sendToUX410();
+    bool paymentInit();
+    bool waitForUX410();
+
+    bool surveyBool;
+    int priceSelect;
 
     DrinkOrder paymentDrinkOrder;
 

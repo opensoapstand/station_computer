1af6e8b840d8a31cb1409a16b37a6e1462925f2c
padraigjriley <padraig@drinkfill.com>
2021-05-14 11:00:39 -0700

    Dispense Before page added

--- ui/dispensepage.cpp
+++ ui/dispensepage.cpp
@@ -63,7 +63,7 @@ dispensePage::~dispensePage()
 void dispensePage::showEvent(QShowEvent *event)
 {
 
-    QPixmap background(":/light/6_dispense_page.png");
+    QPixmap background(":/light/5_dispense_page_before.png");
     background = background.scaled(this->size(), Qt::IgnoreAspectRatio);
     QPalette palette;
     palette.setBrush(QPalette::Background, background);

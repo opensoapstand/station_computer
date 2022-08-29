6ea63a2a31f851b294003b2fb3dfcfe25c7060cf
lode <lode@drinkfill.com>
2022-08-29 11:59:45 -0700

    Set screen orientation at each reboot

--- ui_execute.sh
+++ ui_execute.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# set screen orientation (settings are not saved at reboot)
+DISPLAY=:0 xrandr -o right
+
 while true
 	do
-	   
 	    DISPLAY=:0 xterm -maximized /home/df-admin/production/bin/DF_UI
         sleep 1
 done

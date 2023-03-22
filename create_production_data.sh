c29a1506d68467e171176fba86b51d9df56b0621
ashwani227 <ashwani@drinkfill.com>
2023-03-22 15:07:44 -0700

    Adding wifi status check as service

--- create_production_data.sh
+++ create_production_data.sh
@@ -19,7 +19,8 @@ cd /home/df-admin/production
 sudo -u df-admin mkdir /home/df-admin/production/logging  
 sudo -u df-admin mkdir /home/df-admin/production/logging/ui  
 sudo -u df-admin mkdir /home/df-admin/production/logging/controller  
-sudo -u df-admin mkdir /home/df-admin/production/logging/transactions  
+sudo -u df-admin mkdir /home/df-admin/production/logging/transactions
+sudo -u df-admin mkdir /home/df-admin/production/logging/wifi  
 sudo -u df-admin mkdir /home/df-admin/production/db
 sudo -u df-admin mkdir /home/df-admin/production/admin
 sudo -u df-admin mkdir /home/df-admin/production/bin
@@ -41,6 +42,7 @@ scp /home/df-admin/drinkfill/ui_soapstand.service /home/df-admin/production/admi
 scp /home/df-admin/drinkfill/ui_execute.sh /home/df-admin/production/admin/ui_execute.sh
 scp /home/df-admin/drinkfill/soapstand_manager.sh /home/df-admin/production/admin/soapstand_manager.sh
 scp /home/df-admin/drinkfill/status_services.sh /home/df-admin/production/admin/status_services.sh
+scp /home/df-admin/drinkfill/check_connectivity.sh /home/df-admin/production/admin/check_connectivity.sh
 
 scp /home/df-admin/drinkfill/aws_operations.sh /home/df-admin/production/admin/aws_operations.sh
 
@@ -48,7 +50,6 @@ scp /home/df-admin/drinkfill/screenshotbot.sh /home/df-admin/production/admin/sc
 scp /home/df-admin/drinkfill/screenshotbot_sequence_printer.txt /home/df-admin/production/admin/screenshotbot_sequence_printer.txt
 scp /home/df-admin/drinkfill/screenshotbot_sequence_qr.txt /home/df-admin/production/admin/screenshotbot_sequence_qr.txt
 scp /home/df-admin/drinkfill/updateDatabase.py /home/df-admin/production/admin/updateDatabase.py
-scp /home/df-admin/drinkfill/stationStatus.py /home/df-admin/production/admin/stationStatus.py
 scp /home/df-admin/drinkfill/missingTransactions.py /home/df-admin/production/admin/missingTransactions.py
 
 # ask for db to be written. By default: NO! 

4671ce4aa8e50c4554f11809f8b5fd7588c3e23d
ashwani227 <ashwani@drinkfill.com>
2023-08-01 17:02:22 -0700

    Adding temperature to the portal

--- admin/check_connectivity.sh
+++ admin/check_connectivity.sh
@@ -5,6 +5,11 @@ database_path="/home/df-admin/production/db/configuration.db"
 query="Select machine_id from machine"
 machineId=$(sqlite3 "$database_path" "$query")
 
+database_temperature = "/home/df-admin/production/db/usage.db"
+getTemperature="Select temperature from temperature ORDER BY time DESC LIMIT 1"
+temperatureValue = $(sqlite3 "$database_temperature" "$getTemperature")
+
+
 # Check Wi-Fi connectivity strength
 wifi_strength=$(nmcli -t dev wifi|grep '*')
 #Check if folder exists or create folder and file if it doesn't exist
@@ -13,7 +18,7 @@ mkdir -p "$folder" && touch "$folder"/logging.txt
 
 #initialize soapstandportal POST request variables
 url="https://soapstandportal.com/api/stationStatus/add"
-payload="{\"MachineSerialNumber\" : \"$machineId\",\"wifiStrength\": \"$wifi_strength\"}"
+payload="{\"MachineSerialNumber\" : \"$machineId\",\"wifiStrength\": \"$wifi_strength\",\"temperature\": \"$temperatureValue\"}"
 response=$(curl -X POST -H "Content-Type: application/json" -d "$payload" "$url")
 
 

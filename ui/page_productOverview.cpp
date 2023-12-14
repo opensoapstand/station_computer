ac127ad10ec4542c3e8767ad34da255c85e45ba8
ashwani227 <ashwani@drinkfill.com>
2023-12-13 16:37:34 -0800

    Lower the button instead of hide

--- ui/page_productOverview.cpp
+++ ui/page_productOverview.cpp
@@ -138,7 +138,7 @@ void page_product_overview::showEvent(QShowEvent *event)
 
     if( p_page_idle->thisMachine->selectedProduct->getPaymentMethod()==PAYMENT_TAP_SERIAL){
         ui->pushButton_continue->raise();
-        ui->pushButton_continue_additional->show();
+        // ui->pushButton_continue_additional->show();
         ui->pushButton_continue_additional->raise();
 
         ui->pushButton_continue->setFixedSize(QSize(360,92));
@@ -148,7 +148,7 @@ void page_product_overview::showEvent(QShowEvent *event)
     }
     else{
         ui->pushButton_continue->raise();
-        ui->pushButton_continue_additional->hide();
+        ui->pushButton_continue_additional->lower();
 
         ui->pushButton_continue->setFixedSize(QSize(740,92));
         p_page_idle->thisMachine->setTemplateTextWithIdentifierToObject(ui->pushButton_continue, "proceed_pay");
@@ -624,7 +624,7 @@ void page_product_overview::check_to_page_email(){
     double selectedPrice = p_page_idle->thisMachine->selectedProduct->getBasePrice();
     double finalPrice = p_page_idle->thisMachine->getPriceWithDiscount(selectedPrice);
     if(finalPrice == 0.0 || selectedPrice== 0.0 ){
-        ui->pushButton_continue_additional->hide();
+        ui->pushButton_continue_additional->lower();
         ui->pushButton_continue->setFixedSize(QSize(740,92));
         p_page_idle->thisMachine->setTemplateTextWithIdentifierToObject(ui->pushButton_continue, "proceed_free");
 

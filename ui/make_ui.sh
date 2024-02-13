f330b7c86daa27da78d9f39a14e0c54f89e17910
lode <lode@drinkfill.com>
2024-02-13 15:08:27 -0800

    bug fix Update qmake file.

--- ui/make_ui.sh
+++ ui/make_ui.sh
@@ -8,6 +8,7 @@ sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils
 mkdir build-ui-cli-debug
 cd build-ui-cli-debug
 qmake ../ui.pro -spec linux-g++ CONFIG+=debug
+cp DF_UI ..
 # qmake ../ui.pro -spec linux-g++ CONFIG+=debug CONFIG+=static
 
 make

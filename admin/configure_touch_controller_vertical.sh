67d9e3d3af05a2b8a9d8306fdce39c2134b78fb5
lode <lode@drinkfill.com>
2024-03-18 16:17:14 -0700

    Adjust script for ILITEK touch controller vertical position

--- admin/configure_touch_controller_vertical.sh
+++ admin/configure_touch_controller_vertical.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-
-
 # configure vertical touch screen for ilitek
 # Will not work with WAYLAND. Only tested with xorg display server
 # set startup in /etc/gdm3/custom.conf --> disable wayland 
 
-id=$(xinput | grep -o -E "ILITEK ILITEK-TP\s+id=." |  awk '{print substr($0, length($0))}')
-echo $id
+id=$(DISPLAY=:0 xinput | grep -o -E "ILITEK ILITEK-TP\s+id=." |  awk '{print substr($0, length($0))}')
+# echo $id
 
-xinput list-props $id | grep Matrix
+if [ -z "$id" ]; then
+    echo "Not an ILITEK ILITEK-TP screen"
+else
+    echo "ILITEK ILITEK-TP screen found. --> manually adjust touch controller coordinates. "
+    xinput set-prop "ILITEK ILITEK-TP" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1  # clockwise.
+    DISPLAY=:0 xinput list-props $id | grep Matrix
+fi
+
+
 # allll bad
 # xinput set-prop 8 "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1   # vert mirrored horizontal mirrored
 # xinput set-prop 8 "Coordinate Transformation Matrix" 0 1 0 1 0 0 0 0 1 # vertical ok, horizontal lmirrored
@@ -28,12 +34,11 @@ xinput list-props $id | grep Matrix
 # 0 0 1
 
 #xinput set-prop 8 "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1  # clockwise.
-xinput set-prop "ILITEK ILITEK-TP" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1  # clockwise.
+# xinput set-prop "ILITEK ILITEK-TP" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1  # clockwise.
 
+# echo "After clockwise transformation:"
 
-echo "After clockwise transformation:"
-
-xinput list-props "ILITEK ILITEK-TP" | grep Matrix
+# xinput list-props "ILITEK ILITEK-TP" | grep Matrix
 
 
 
